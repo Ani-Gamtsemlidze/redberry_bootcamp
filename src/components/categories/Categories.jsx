216edd8906d99a04236b5e9ae9b7e3db08493b72
User-s <ani.gamcemlidze20@gmail.com>
2023-12-23 01:14:58 +0400

    categories border styling

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { useBlogs } from "../../context/BlogContextProvider";
 import DataFetcher from "../../utilis/DataFetcher";
 import styles from "./Categories.module.css";
@@ -8,47 +7,41 @@ const BASE_URL = "https://api.blog.redberryinternship.ge/api/categories";
 
 function Categories() {
   const [categoryId, setCategoryId] = useState([]);
-  const { blogsList, filterHandler } = useBlogs();
+  const [activeIds, setActiveIds] = useState([]);
+  const { filterHandler } = useBlogs();
 
-  // const navigate = useNavigate();
-  // const [searchParams] = useSearchParams();
-  // console.log(searchParams.get("categoryId"));
   const { data } = DataFetcher(BASE_URL);
-  // if(categoryId.length)
+
   function handlerId(id) {
-    // const queryString = categoryId
-    //   .map((id, index) => `?categoryId${index + 1}=${id}`)
-    //   .join("&");
-    // navigate(`/${queryString}`);
-    setCategoryId((prevIds) => {
-      if (prevIds.includes(id)) {
-        return prevIds.filter((existingId) => existingId !== id);
+    setActiveIds((prevActiveIds) => {
+      if (prevActiveIds.includes(id)) {
+        return prevActiveIds.filter((activeId) => activeId !== id);
       } else {
-        return [...prevIds, id];
+        return [...prevActiveIds, id];
       }
     });
   }
-  useEffect(() => {
-    // const queryString = categoryId
-    //   .map((id, index) => `?categoryId${index + 1}=${id}`)
-    //   .join("&");
-    // navigate(`/${queryString}`);
 
-    filterHandler(categoryId);
-  }, [categoryId]);
+  useEffect(() => {
+    filterHandler(activeIds);
+  }, [activeIds]);
 
   return (
     <ul className={styles.category_box}>
       {data.data?.map((category) => (
         <li
           onClick={() => handlerId(category.id)}
-          className={styles.category_list}
+          className={
+            activeIds.includes(category.id)
+              ? `${styles.category_list} active_category`
+              : styles.category_list
+          }
           style={{
             backgroundColor: `${category.background_color}`,
           }}
           key={category.id}
         >
-          <p style={{ color: category.text_color }}>{category.title} </p>
+          <p style={{ color: category.text_color }}>{category.title}</p>
         </li>
       ))}
     </ul>
