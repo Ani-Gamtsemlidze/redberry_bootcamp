1d405f60bb0b3e2af56b027804acbf245af0d595
User-s <ani.gamcemlidze20@gmail.com>
2023-12-29 20:32:53 +0400

    save categories in local storage

--- src/components/form/Select/Select.jsx
+++ src/components/form/Select/Select.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputLabel from "@mui/material/InputLabel";
@@ -37,14 +36,25 @@ let catArray = [
   { id: 13, title: "სამედიცინო", background_color: "#FFBB2F" },
   { id: 14, title: "სხვა", background_color: "#FA5757" },
 ];
+
+let CatId = JSON.parse(localStorage.getItem("form_values"))?.category_input;
+let filteredCategories = catArray.filter((category) =>
+  CatId.includes(category.id)
+);
+
+// Extract titles from filteredCategories
+let titles = filteredCategories.map((category) => category.title);
+
 export default function MultipleSelectChip({ selectArray, label }) {
   const { handleInputChange } = useUpload();
-  const [listName, setListName] = React.useState([]);
+  const [listName, setListName] = React.useState(titles || []);
+
   const handleChange = (e) => {
     handleInputChange(e);
     const {
       target: { value },
     } = e;
+
     setListName(typeof value === "string" ? value.split(",") : value);
   };
 
@@ -55,6 +65,20 @@ export default function MultipleSelectChip({ selectArray, label }) {
     });
 
     setListName(deleteData);
+
+    let filteredCategories = catArray.filter((category) =>
+      deleteData.includes(category.title)
+    );
+
+    // Extract IDs from filteredCategories
+    let ids = filteredCategories.map((category) => category.id);
+
+    let storedFormValues =
+      JSON.parse(localStorage.getItem("form_values")) || {};
+
+    storedFormValues.category_input = JSON.stringify(ids);
+
+    localStorage.setItem("form_values", JSON.stringify(storedFormValues));
   };
 
   const bgArray = () => {
@@ -78,7 +102,7 @@ export default function MultipleSelectChip({ selectArray, label }) {
         <Select
           className="upload_select"
           labelId="demo-multiple-chip-label"
-          id="demo-multiple-chip-label"
+          id="demo-multiple-chip"
           multiple
           value={listName}
           onChange={handleChange}
